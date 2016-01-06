e7575507898c0b15348496b142ad8e2a01bebf52
Cody Ray Freeman Hoeft <crhoeft@gmail.com>
2016-01-05 22:36:16 -0800

    Add SemDiff.Test project, add small logger and a corisponding test

--- SemDiff.Test/Properties/AssemblyInfo.cs
+++ SemDiff.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SemDiff.Core")]
-[assembly: AssemblyDescription("Library containing SemDiff's core functionality")]
+[assembly: AssemblyTitle("SemDiff.Test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SemDiff.Core")]
+[assembly: AssemblyProduct("SemDiff.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9c4644d-f6d0-4637-bc77-7527fdd77da8")]
+[assembly: Guid("e93760c1-be7e-4f05-9fb5-19781df2e7b4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,8 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1")]
-// Use Stamp to insert git information into assembly info
-[assembly: AssemblyInformationalVersion("%version2% Head=%branch% Sha=%githash%")]
-[assembly: InternalsVisibleTo("SemDiff.Test")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
