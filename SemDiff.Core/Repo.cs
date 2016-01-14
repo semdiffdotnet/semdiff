246ef898b813e51ee9353c6b65f7c9d5c1b03d59
Cody Ray Freeman Hoeft <crhoeft@gmail.com>
2016-01-13 19:03:45 -0800

    Implement logic for getting repo owner and name

--- SemDiff.Core/Repo.cs
+++ SemDiff.Core/Repo.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace SemDiff.Core
 {
@@ -8,10 +11,66 @@ namespace SemDiff.Core
     /// </summary>
     public class Repo
     {
+        private static readonly ConcurrentDictionary<string, Repo> _repoLookup = new ConcurrentDictionary<string, Repo>();
+
         public static Repo GetRepoFor(string filePath)
         {
-            //Code for much of this is in prototypes
-            throw new NotImplementedException();
+            return _repoLookup.GetOrAdd(Path.GetDirectoryName(filePath), AddRepo);
+        }
+
+        internal static Repo AddRepo(string directoryPath)
+        {
+            Logger.Debug($"Dir: {directoryPath}");
+            var gitconfig = Path.Combine(directoryPath, ".git", "config");
+            if (File.Exists(gitconfig))
+            {
+                Logger.Info($"Git Config File Found: {gitconfig}");
+                return RepoFromConfig(gitconfig);
+            }
+            else
+            {
+                //Go up a directory and check it out
+                var parentDirectory = Path.GetDirectoryName(directoryPath);
+                if (parentDirectory == null)
+                {
+                    //This file is not in a git repo! (GetDirectoryName returns null when given the root directory)
+                    return null; //This is much more common than you might think, because offten random files are compiled, this will allow us to exclude them
+                }
+                return _repoLookup.GetOrAdd(parentDirectory, AddRepo);
+            }
+        }
+
+        private static readonly Regex _gitHubUrl = new Regex(@"(git@|https:\/\/)github\.com(:|\/)(.*)\/(.*)\.git");
+
+        public string Owner { get; private set; }
+        public string Name { get; private set; }
+
+        internal static Repo RepoFromConfig(string gitconfigPath)
+        {
+            var config = File.ReadAllText(gitconfigPath);
+            var match = _gitHubUrl.Match(config);
+            if (!match.Success)
+                throw new NotImplementedException("Git repo doesn't seem to be a GitHub repository");
+
+            var url = match.Value;
+            var owner = match.Groups[3].Value;
+            var name = match.Groups[4].Value;
+            Logger.Debug($"Repo: Owner='{owner}' Name='{name}' Url='{url}'");
+            return new Repo(owner, name);
+        }
+
+        /// <summary>
+        /// Flushes the internal mapings of directories to repos
+        /// </summary>
+        internal static void ClearLookup()
+        {
+            _repoLookup.Clear();
+        }
+
+        internal Repo(string owner, string name)
+        {
+            Owner = owner;
+            Name = name;
         }
 
         /// <summary>
---
+++ SemDiff.Test/GetRepoTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SemDiff.Core;
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace SemDiff.Test
+{
+    [TestClass]
+    public class GetRepoTests
+    {
+        [TestMethod]
+        public void RepoFromConfigTest()
+        {
+            var repo = Repo.RepoFromConfig("testgitconfig.txt");
+            Assert.AreEqual("dotnet", repo.Owner);
+            Assert.AreEqual("roslyn", repo.Name);
+        }
+
+        [TestMethod]
+        public void RepoFromConfig2Test()
+        {
+            var repo = Repo.RepoFromConfig("testgitconfig2.txt");
+            Assert.AreEqual("haroldhues", repo.Owner);
+            Assert.AreEqual("HaroldHues-Public", repo.Name);
+        }
+
+        [TestMethod]
+        public void GetRepoForThisFileTest()
+        {
+            var thisFile = GetFileName();
+            var repo = Repo.GetRepoFor(thisFile);
+            Assert.AreEqual("semdiffdotnet", repo.Owner);
+            Assert.AreEqual("semdiff", repo.Name);
+        }
+
+        private string GetFileName([CallerFilePath] string file = "") => file;
+
+        [TestMethod]
+        public void GetRepoThatDoesntExistTest()
+        {
+            var inAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Local", "Microsoft");
+            var repo = Repo.GetRepoFor(inAppData);
+            Assert.AreEqual(null, repo);
+        }
+    }
+}
