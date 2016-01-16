0b1b48279739ae2c632e089677fe8a994037d2ab
MRose90 <rosem2@onid.orst.edu>
2016-01-15 18:38:27 -0800

    Added a test for the pull request list
    Test is incomplete, but file has been created.

---
+++ SemDiff.Test/PullRequestListTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SemDiff.Core;
+using System.Collections.Generic;
+namespace SemDiff.Test
+{
+    [TestClass]
+    public class PullRequestListTest
+    {
+        [TestMethod]
+        public void PullRequestFromTestRepo()
+        {
+            var gh = new GitHub("haroldhues", "curly-broccoli");
+            var req = gh.GetPullRequests();
+        }
+    }
+}
