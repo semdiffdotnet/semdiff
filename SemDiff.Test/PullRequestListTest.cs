39e3582eb992fe4e0cc047261c2999f23872815e
MRose90 <rosem2@onid.orst.edu>
2016-01-22 09:50:17 -0800

    Tests for GitHub.cs
    3 Tests. One tests that the GitHub object is initialized correctly. One
    tests that the PullRequests are retrieved correctly. One tests that that
    the files are created and saved correctly.

--- SemDiff.Test/PullRequestListTest.cs
+++ SemDiff.Test/PullRequestListTest.cs
@@ -1,17 +1,115 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SemDiff.Core;
+using System;
 using System.Collections.Generic;
+
 namespace SemDiff.Test
 {
     [TestClass]
     public class PullRequestListTest
     {
+        string owner = "semdiffdotnet";
+        string repository = "curly-broccoli";
+        GitHub github;
+
+        public void setGitHub()
+        {
+            github = new GitHub(owner, repository);
+        }
+        [TestMethod]
+        public void NewGitHub()
+        {
+            setGitHub();
+            Assert.AreEqual(github.RepoName, repository);
+            Assert.AreEqual(github.RepoOwner, owner);
+
+        }
         [TestMethod]
         public void PullRequestFromTestRepo()
         {
-            var gh = new GitHub("semdiffdotnet", "curly-broccoli");
-            var req = gh.GetPullRequests();
+            setGitHub();
+            var requests = github.GetPullRequests();
+            var fourWasFound = false;
+            if (github.RequestsRemaining != 0)
+            {
+                foreach(var r in requests)
+                {
+                   if(r.number == 4)
+                    {
+                        fourWasFound = true;
+                        Assert.AreEqual(r.locked,false);
+                        Assert.AreEqual(r.state, "open");
+                        Assert.AreEqual(r.user.login, "haroldhues");
+                        Assert.AreEqual(r.files.Count, 1);
+                        foreach(var f in r.files)
+                        {
+                            Assert.AreEqual(f.filename, "Curly-Broccoli/Broccoli/AnalyseUser.cs");
+                            Assert.AreEqual(f.raw_url, "https://github.com/haroldhues/curly-broccoli/raw/79336452724314e1fb29671a7319db9d9a5c7e29/Curly-Broccoli/Broccoli/AnalyseUser.cs");
+                        }
+                    }
+                }
+                Assert.AreEqual(fourWasFound, true);
+            }
+        }
+        [TestMethod]
+        public void GetFilesFromGitHub()
+        {
+            setGitHub();
+            var requests = github.GetPullRequests();
+            var fourWasFound = false;
+            if (github.RequestsRemaining != 0)
+            {
+                Assert.AreNotEqual(github.RequestsRemaining, 0);
+                foreach (var r in requests)
+                {
+                    github.DownloadFiles(r);
+                    if (r.number == 4)
+                    {
+                        fourWasFound = true;
+                        string line;
+                        int counter = 0;
+                        string[] directoryTokens;
+                        string dir = "";
+                        foreach (var f in r.files)
+                        {
+                            directoryTokens = f.filename.Split('/');
+                            dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/SemDiff/";
+                            foreach (var token in directoryTokens)
+                            {
+                                if (token != directoryTokens[0])
+                                {
+                                    dir = dir + "/" + token;
+                                }
+                                else
+                                {
+                                    dir = dir + token + "/" + r.number;
+                                }
+                            }
+                        }
+                        using (var file = new System.IO.StreamReader(dir))
+                        {
+                            while ((line = file.ReadLine()) != null)
+                            {
+                                if (counter == 6)
+                                {
+                                    var expected = "namespace Curly";
+                                    Assert.AreEqual(line, expected);
+                                }
+                                if (counter == 9)
+                                {
+                                    var expected = "    /// Utility for logging to an internal list of Log entities";
+                                    Assert.AreEqual(line, expected);
+                                }
+                                counter++;
+                            }
+
+                            file.Close();
+                        }
+                        Assert.AreEqual(counter, 35);
+                    }
+                }
+                Assert.AreEqual(fourWasFound, true);
+            }
         }
     }
 }
