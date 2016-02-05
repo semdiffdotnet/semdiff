ee79bfcccb2fdfd9e04fa8d2cb4f6de0878101ef
Cody Ray Freeman Hoeft <crhoeft@gmail.com>
2016-02-04 21:54:03 -0800

    Stub out Diagnostics and Analysis classes

---
+++ SemDiff.Core/DetectedFalsePositive.cs
@@ -0,0 +1,9 @@
+﻿namespace SemDiff.Core
+{
+    /// <summary>
+    /// This class will hold all the important information about what kind of False Positive was detected and the files, pull requests, and changes involved
+    /// </summary>
+    public class DetectedFalsePositive
+    {
+    }
+}
---
+++ SemDiff.Core/Diagnostics.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace SemDiff.Core
+{
+    /// <summary>
+    /// This class provides our interface for reporting diagnostics using roslyn
+    /// </summary>
+    public class Diagnostics
+    {
+        /// <summary>
+        /// The Diagnostics we support, provided for the SupportedDiagnostics property of DiagnosticAnalyzer
+        /// </summary>
+        public static ImmutableArray<DiagnosticDescriptor> Supported { get; internal set; }
+
+        /// <summary>
+        /// Converts `DetectedFalsePositive`s to Diagnostics (the class provided by Roslyn) and sends them to the function provided
+        /// </summary>
+        public static void Report(IEnumerable<DetectedFalsePositive> fps, Action<Diagnostic> reporter)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converts `DetectedFalseNegative`s to Diagnostics (the class provided by Roslyn) and sends them to the function provided
+        /// </summary>
+        public static void Report(IEnumerable<DetectedFalseNegative> fps, Action<Diagnostic> reporter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
